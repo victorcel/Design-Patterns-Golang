5eaaa460ea6c2b304f15899b940568fc27dfb5ba
Victor Elias Barrera Florez <vbarrera@vozy.co>
2022-11-07 09:24:45 -0500

    feat: wire dependency injection

---
+++ wire/wire.go
@@ -0,0 +1,11 @@
+//go:build wireinject
+// +build wireinject
+
+package main
+
+import "github.com/google/wire"
+
+func InitializeEvent() Event {
+	wire.Build(GetMessage, GetGreeter, GetEvent)
+	return Event{}
+}
